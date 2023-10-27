416cc2a97f87aaeed148cb701ffcbc25e3c78251
viniciius14 <rodri.f.ribeiro@gmail.com>
2023-10-27 18:53:17 +0100

    Memory.c implementation

---
+++ src/utils/memory.c
@@ -0,0 +1,36 @@
+#include "types.h"
+#include "memory.h"
+
+/* Sets n bytes of memory to value starting at address dst */
+static inline void memset(void *dst, uint8_t value, size_t n) {
+    uint8_t *d = dst;
+    while(n-- > 0){
+        *(d++) = value;
+    }
+}
+
+/* Copies n bytes of memory from src to dst */
+static inline void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = dst;
+    const uint8_t *s = src;
+
+    while(n-- > 0){
+        *(d++) = *(s++);
+    }
+}
+
+/**/
+static inline void  *memmove(void *dst, const void *src, size_t n) {
+    if(src > dst) {
+        return memcpy(dst, src, n);
+    }
+
+    uint8_t *d = dst;
+    const uint8_t *s = src;
+
+    for (size_t i = n; i > 0; i--) {
+        d[i - 1] = s[i - 1];
+    }
+
+    return dst;
+}
