7bbd4d4c0d0f02f02d485df4697ba94b3156403d
viniciius14 <rodri.f.ribeiro@gmail.com>
2023-10-27 18:52:34 +0100

    Memory.c implementation

--- include/utils/memory.h
+++ include/utils/memory.h
@@ -1,7 +1,10 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-memset();
-memclr();
+#include "types.h"
+
+static inline void  memset(void *dst, uint8_t value, size_t n);
+static inline void  *memcpy(void *dst, const void *src, size_t n);
+static inline void  *memmove(void *dst, const void *src, size_t n);
 
 #endif /* MEMORY_H */
