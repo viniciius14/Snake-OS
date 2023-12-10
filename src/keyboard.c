598d0a3d3e78657a920a58e24466bd248854a614
viniciius14 <rodri.f.ribeiro@gmail.com>
2023-12-10 19:35:49 +0000

    Working on keyboard driver

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,16 +1,16 @@
 #include "keyboard.h"
 
-
-// uint8_t keyboard_layout_us[] = {
-//     KEY_NULL, KEY_ESC, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-//     '-', '=', KEY_BACKSPACE, KEY_TAB, 'q', 'w', 'e', 'r', 't', 'y', 'u',
-//     'i', 'o', 'p', '[', ']', KEY_ENTER, 0, 'a', 's', 'd', 'f', 'g', 'h', 'j',
-//     'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
-//     ',', '.', '/', 0, 0, 0, ' ', 0, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5,
-//     KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, 0, 0, KEY_HOME, KEY_UP,
-//     KEY_PAGE_UP, '-', KEY_LEFT, '5', KEY_RIGHT, '+', KEY_END, KEY_DOWN,
-//     KEY_PAGE_DOWN, KEY_INSERT, KEY_DELETE, 0, 0, 0, KEY_F11, KEY_F12
-// };
+/* Derived from the info in https://kbdlayout.info/kbdpo/virtualkeys , also where is key 55? */
+uint8_t keyboard_layout_pt[] = {
+    0, KEY_ESC,
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'', KEY_OEM6, KEY_BACKSPACE,
+    KEY_TAB, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '+', KEY_OEM1, KEY_ENTER,
+    KEY_LCTRL, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', KEY_OEM3, KEY_OEM7, '~',
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '-', KEY_R_SHIFT,
+    '*', KEY_L_ALT, ' ', KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_NUM_LOCK,
+    KEY_SCROLL_LOCK, '7','8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.',
+    KEY_PRT_SC, 0, '<', KEY_F11, KEY_F12
+};
 
 void init_keyboard(void) {
     idt_set_descriptor(33, keyboard_handler, INT_GATE_FLAGS);
@@ -27,6 +27,8 @@ void keyboard_handler(int_frame_32_t *frame) {
     uint8_t key = inb(0x60);
 
     /* Convert value to corresponding keyboard key */
+    key = keyboard_layout_pt[key];
+
     k_print((char *)&key);
     send_pic_eoi(1);
 }
--- src/keyboard.h
+++ src/keyboard.h
@@ -7,6 +7,30 @@
 
 #define PS2_DATA_PORT   0x60
 
+#define KEY_ENTER       '\n'
+#define KEY_ESC         0x01
+#define KEY_OEM6        0x0D
+#define KEY_OEM1        0x1B
+#define KEY_BACKSPACE   0x0E
+#define KEY_TAB         0x0F
+#define KEY_LCTRL       0x1D
+#define KEY_OEM3        0x27
+#define KEY_OEM7        0x28
+#define KEY_LSHIFT      0x2A
+#define KEY_R_SHIFT     0x37
+#define KEY_L_ALT       0x38
+#define KEY_F6          0x40
+#define KEY_F7          0x41
+#define KEY_F8          0x42
+#define KEY_F9          0x43
+#define KEY_F10         0x44
+#define KEY_NUM_LOCK    0x45
+#define KEY_SCROLL_LOCK 0x46
+#define KEY_PRT_SC      0x54
+#define KEY_F11         0x57
+#define KEY_F12         0x58
+
+
 typedef struct {
     uint8_t key;
     bool    shift;
