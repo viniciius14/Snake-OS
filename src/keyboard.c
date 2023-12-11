61cf86c2bd901991cdbbafcdb92ad34e0bc67277
Rodrigo Ribeiro <rodri.f.ribeiro@gmail.com>
2023-12-11 14:17:13 +0000

    Update keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,6 +1,5 @@
 #include "keyboard.h"
-
-/* Derived from the info in https://kbdlayout.info/kbdpo/virtualkeys , also where is key 55? */
+/*
 uint8_t keyboard_layout_pt[] = {
     0, KEY_ESC,
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'', KEY_OEM6, KEY_BACKSPACE,
@@ -11,13 +10,13 @@ uint8_t keyboard_layout_pt[] = {
     KEY_SCROLL_LOCK, '7','8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.',
     KEY_PRT_SC, 0, '<', KEY_F11, KEY_F12
 };
-
+*/
 void init_keyboard(void) {
     idt_set_descriptor(33, keyboard_handler, INT_GATE_FLAGS);
-
     /* Clear out keyboard buffer */
-    while(inb(0x64) & 1) inb(0x60);
-
+    while(inb(0x64) & 1) {
+        inb(0x60);
+    }
     clear_irq_mask(1);
 }
 
@@ -25,14 +24,9 @@ void keyboard_handler(int_frame_32_t *frame) {
     (void)frame;
     /* Get value being sent to port */
     uint8_t key = inb(0x60);
-
+    k_print_hex(key);
     /* Convert value to corresponding keyboard key */
-    key = keyboard_layout_pt[key];
-
-    k_print((char *)&key);
+    //key = keyboard_layout_pt[key];
+    //k_print((char *)&key);
     send_pic_eoi(1);
 }
-
-
-// uint8_t get_key(void) {
-// }
